1cf25f37bd13606a5290c347517f3c0cfd9261ec
Joseph Mercado <JosephMercado475@gmail.com>
2021-03-03 01:02:34 -0600

    npm packages were updated to fix vuner

--- server/api.js
+++ server/api.js
@@ -2,6 +2,46 @@ let Db = require('./dboperations')
 let Customer = require('./customer')
 const dboperations = require('./dboperations')
 
+
+let express = require('express')
+let bodyParser = require('body-parser')
+let cors = require('cors')
+const { response } = require('express')
+let app = express()
+let router = express.Router();
+
+app.use(bodyParser.urlencoded({extended: true}))
+
+app.use(bodyParser.json())
+app.use(cors())
+app.use('/api', router)
+
+
+
+router.use((request,response,next) => {
+        console.log('middleware')
+        next()
+})
+
+
+
+
+router.route('/customer').get((request,response)=>{
+    dboperations.getCustomer().then(result => {
+        //console.log(result)
+        response.json(result[0])
+    })
+
+}) 
+
+
+let port = process.env.PORT || 8090
+app.listen(port)
+console.log('Customer API is running at ' + port)
+
+
+
+
 dboperations.getCustomer().then(result => {
     console.log(result);
 })
